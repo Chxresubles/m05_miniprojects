716baf405cf3ff70166d0d5ecc09fca5bf46f742
Chxresubles <favre.le@hotmail.com>
2022-02-28 11:22:11 +0100

    ADD: Add algorithm file for the wine quality

---
+++ wine_quality/algorithm.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""
+ML algorithms for the wine quality dataset.
+
+This script contains two ML algorithms for
+predicting wine quality in functions of
+the wine intrinsic properties:
+- Linear regression
+- Regression trees
+"""
+
+
+# ============================================================================================================
+# Imports
+# ============================================================================================================
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+
+# ============================================================================================================
+# Constants
+# ============================================================================================================
+
+
+# ============================================================================================================
+# Useful functions
+# ============================================================================================================
+
+def LR(train_set, test_set):
+    """Run training and testing and the wine quality set using Linear Regression."""
+    # LR training
+    # LR testing
+    pass
+
+
+def RT(train_set, test_set):
+    """Run training and testing and the wine quality set using Regression Trees."""
+    # RT training
+    # RT testing
+    pass
+
+
+def trainAndTest(method, preprocessing):
+    # Load train and test dataset
+    # Preprocessing
+    # Method run training and testing
+    # Analysis
+    pass
