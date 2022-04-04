76043a713d480d571262c5c0935323adfdaedd0d
Chxresubles <favre.le@hotmail.com>
2022-04-04 15:41:11 +0200

    FIX: Fix test init files

--- test/test_boston_house_price/__init__.py
+++ test/test_boston_house_price/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-"""Init file for utils test folder."""
+"""Init file for boston_house_price test folder."""
 
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "m05"))
--- test/test_utils/__init__.py
+++ test/test_utils/__init__.py
@@ -5,4 +5,4 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "m05"))
